32e2f2b7d863846193d67328638f10c3d5730186
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-12-08 00:25:06 +0700

    get Started Page

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spotify_clone/core/configs/assets/app_vector.dart';
+import 'package:flutter_spotify_clone/presentation/intro/pages/get_started.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: SvgPicture.asset(AppVector.logo),
+      ),
+    );
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => const GetStartedPage(),
       ),
     );
   }

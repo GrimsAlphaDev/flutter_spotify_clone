72d061faf5fb903aac7b7b7828072989df064472
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-12-08 16:28:46 +0700

    cloud fire store conn, and storing default song

--- lib/domain/usecases/auth/signin.dart
+++ lib/domain/usecases/auth/signin.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_spotify_clone/core/usecase/usecase.dart';
-import 'package:flutter_spotify_clone/data/models/auth/create_user_req.dart';
 import 'package:flutter_spotify_clone/data/models/auth/signin_user_req.dart';
 import 'package:flutter_spotify_clone/domain/repository/auth/auth.dart';
 import 'package:flutter_spotify_clone/service_locator.dart';
